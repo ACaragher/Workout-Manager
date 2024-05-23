02db4def6e0e87bbfaed406bd64da4e15c68717b
Adam Caragher <caragher.adam@gmail.com>
2024-05-23 20:58:12 +0100

    Project setup

---
+++ src/test/java/ie/caragher/workoutmanager/WorkoutmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.caragher.workoutmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
